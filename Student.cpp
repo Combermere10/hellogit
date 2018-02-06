186592cc8d92e137df9fd89a5446b38e727a7bb2
Combermere10 <Combermere10@users.noreply.github.com>
2018-02-06 09:51:12 -0400

    Added header and implementation file

---
+++ Student.cpp
@@ -0,0 +1,26 @@
+#include "Student.h"
+
+Student::Student()
+{
+
+}
+
+Student::getName()
+{
+	return StudentName;
+}
+
+Student::setName(string name)
+{
+	StudentName = name;
+}
+
+Student::getAge()
+{
+	return StudentAge;
+}
+
+Student::setAge(int Age)
+{
+	StudentAge = Age;
+}
---
+++ Student.h
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include<string>
+using namespace std;
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+class Student
+{
+	private:
+			string StudentName;
+			int StudentAge;
+	public:
+	student();
+	getName();
+	getAge();
+	setName(string);
+	setAge(int);
+}
+
+
+
+#endif
